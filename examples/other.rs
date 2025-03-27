f9ad15e4b0237e6cc811452f6931f2e939881090
sleepy ramen <rllola80@gmail.com>
2025-03-27 18:31:56 +0100

    feat: added examples with profiling functionality (#3)

---
+++ examples/other.rs
@@ -0,0 +1,24 @@
+use merkle_other::MerkleTree;
+use rand::RngCore;
+use ring::digest::{Algorithm, SHA256};
+use std::time::Instant;
+
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+static DIGEST: &Algorithm = &SHA256;
+
+fn main() {
+    let _profiler = dhat::Profiler::new_heap();
+    let start = Instant::now();
+
+    let mut contents = vec![vec![0u8; 256]; 100000];
+    let mut rng = rand::rng();
+
+    for mut v in &mut contents {
+        rng.fill_bytes(&mut v);
+    }
+
+    let _mtree = MerkleTree::from_vec(DIGEST, contents);
+
+    println!("Time elapsed : {:?}", start.elapsed());
+}
